c1cb8e3435847fe5980b32e36f3aa55070be05a7
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-07-05 17:15:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🔍 Channel', url='https://t.me/Any_movie_1'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/Badsha_Studios')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
@@ -87,7 +87,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🔍 Channel', url='https://t.me/Any_movie_1'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/Badsha_Studios')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
