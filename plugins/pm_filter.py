56d3898da1c022e41c3ea74d524069f90976f60b
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-07-07 18:18:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,11 +30,11 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 
-@Client.on_message()
+@Client.on_message(filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
-        await auto_filter(client, message)
+        await auto_filter()
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
